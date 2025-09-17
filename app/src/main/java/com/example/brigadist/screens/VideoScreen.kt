1bdf93aee876944e51fb45bce7158b4dbe015215
ca.medina1 <ca.medina1@uniandes.edu.co>
2025-09-17 13:57:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brigadist/screens/VideoScreen.kt
@@ -0,0 +1,18 @@
+package com.example.brigadist.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun VideoScreen(padding: PaddingValues) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(padding),
+    ) {
+        Text("Videos Screen")
+    }
+}
